989c36115a8e031f9763bb523c13c2fab4f499c0
Tralfamadoran97 <peterfor42@gmail.com>
2025-12-02 20:07:56 -0500

    SLDSC Plots (#199)
    - Add a task to plot the output of Stratified Linkage Disequilibrium
    Score Regression
    - Apply this task to the LDL GWAS from the million veteran program.
    This produces the following plot, showing the focus of the GWAS signal
    in the liver:
    <img width="1520" height="809" alt="ldl_sldsc"
    src="https://github.com/user-attachments/assets/1c11a53e-957a-4f6e-95dc-247e451b974a"
    />

---
+++ mecfs_bio/assets/reference_data/linkage_disequilibrium_score_reference_data/from_papers/finucane_2018_franke_gtex_categories.py
@@ -0,0 +1,33 @@
+"""
+Categories for tissue and cell types from the GTEx and Franke lab datasets
+
+source:
+Supplementary material to
+Finucane, Hilary K., et al. "Heritability enrichment of specifically expressed genes identifies disease-relevant tissues and cell types." Nature genetics 50.4 (2018): 621-629.
+
+"""
+
+from pathlib import PurePath
+
+from mecfs_bio.build_system.meta.read_spec.dataframe_read_spec import (
+    DataFrameReadSpec,
+    DataFrameTextFormat,
+)
+from mecfs_bio.build_system.meta.reference_meta.reference_file_meta import (
+    ReferenceFileMeta,
+)
+from mecfs_bio.build_system.task.download_file_task import DownloadFileTask
+
+FICUANE_2018_FRANKE_GTEX_CATEGORIES = DownloadFileTask(
+    meta=ReferenceFileMeta(
+        asset_id="ficuane_2018_combined_gtex_franke_categories",
+        group="linkage_disequilibrium_score_regression_aux_data",
+        sub_group="data_from_papers",
+        sub_folder=PurePath("ficuane_2018"),
+        filename="ficuane_2018_combined_gtex_franke_categories.csv",
+        extension=".csv",
+        read_spec=DataFrameReadSpec(DataFrameTextFormat(separator=",")),
+    ),
+    url="https://www.dropbox.com/scl/fi/2cr5ipddzkde6rj33tcxk/Finucane_2018_combined_gtex_franke.csv?rlkey=w5knnradpjugl2l1orml9j0g9&dl=1",
+    md5_hash="3f121ae8936e380828a14dc76c2fd309",
+)
