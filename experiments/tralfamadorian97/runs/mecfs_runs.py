485965b6d99ac2779635609c4db4bd0148e0928c
Tralfamadoran97 <peterfor42@gmail.com>
2026-02-05 17:15:22 -0500

    Harmonization with SUSIE (#422)
    - Add a new task to harmonize a GWAS with reference data using allele
    matching instead of rsids.
    - Add a new task to fine map a GWAS locus using SUSIE.
    - Apply these tasks to the chromosome 1 GWAS locus of DecodeME

--- experiments/tralfamadorian97/runs/mecfs_runs.py
+++ experiments/tralfamadorian97/runs/mecfs_runs.py
@@ -1,3 +1,6 @@
+"""
+Rough experimental scripts pertaining to analysis of ME/CFS data
+"""
 
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 
@@ -6,17 +9,29 @@ from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_region_plot_BTN1A
     DECODE_ME_BTN1A1_REGION_PLOT_37
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_region_plot_rabgap1l_locus_37 import \
     DECODE_ME_RABGAP1L_REGION_PLOT_37
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.susie_finemap_decode_me_37_chr1_173_locus import \
+    DECODE_ME_GWAS_1_SUSIE_FINEMAP_CHR1_173_000_001_LOCUS
 from mecfs_bio.assets.gwas.me_cfs.decode_me.processed_gwas_data.decode_me_annovar_37_rsids_assignment import \
     DECODE_ME_GWAS_1_37_ANNOVAR_DBSNP150_RSID_ASSIGNED
+from mecfs_bio.assets.gwas.me_cfs.decode_me.processed_gwas_data.prep_for_fine_mapping.chr1_173_locus.harmonize_with_polyfun_reference_alleles import \
+    DECODE_ME_HARMONIZE_WITH_CHR1_173_000_001_LD_VIA_ALLELES
+from mecfs_bio.assets.gwas.me_cfs.decode_me.processed_gwas_data.prep_for_fine_mapping.chr1_173_locus.harmonize_with_polyfun_reference_rsid import \
+    DECODE_ME_HARMONIZE_WITH_CHR1_173_000_001_LD
 from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.custom.roadmap_cell_type_categorization import \
     ROADMAP_CELL_TYPE_CATEGORIES_FOR_LDSC
+from mecfs_bio.assets.reference_data.ukbb_ld_matrices.from_polyfun.chr1_173000001.chr1_173000001_176000001_labels import \
+    CHR1_173000001_17600000_UKBB_LD_LABELS_DOWNLOAD
 
 
 def run_initial_decode_me_analysis():
     DEFAULT_RUNNER.run(
         [
             # DECODE_ME_RABGAP1L_REGION_PLOT_37,
-            DECODE_ME_GWAS_1_37_ANNOVAR_DBSNP150_RSID_ASSIGNED.harmonize_task
+            # DECODE_ME_GWAS_1_37_ANNOVAR_DBSNP150_RSID_ASSIGNED.harmonize_task
+            # CHR1_173000001_17600000_UKBB_LD_LABELS_DOWNLOAD
+            # DECODE_ME_HARMONIZE_WITH_CHR1_173_000_001_LD
+            # DECODE_ME_HARMONIZE_WITH_CHR1_173_000_001_LD_VIA_ALLELES
+            DECODE_ME_GWAS_1_SUSIE_FINEMAP_CHR1_173_000_001_LOCUS
          # DECODE_ME_BTN1A1_REGION_PLOT_37
          #    DECODE_ME_BTN1A1_REGION_PLOT_37
          #    ROADMAP_CELL_TYPE_CATEGORIES_FOR_LDSC
@@ -26,6 +41,9 @@ def run_initial_decode_me_analysis():
         # DECODE_ME_BASIC_CIS_PQTL_MR.terminal_tasks(),
         incremental_save=True,
         must_rebuild_transitive=[
+
+            DECODE_ME_GWAS_1_SUSIE_FINEMAP_CHR1_173_000_001_LOCUS
+            # DECODE_ME_HARMONIZE_WITH_CHR1_173_000_001_LD
             # DECODE_ME_BASIC_CIS_PQTL_MR.multiple_testing_task
             # DECODE_ME_MASTER_GENE_LIST_AS_MARKDOWN,
             # DECODE_ME_MASTER_GENE_LIST_WITH_GGET
--- mecfs_bio/build_system/task/harmonize_gwas_with_reference_table_via_rsid.py
+++ mecfs_bio/build_system/task/harmonize_gwas_with_reference_table_via_rsid.py
@@ -8,7 +8,13 @@ from attrs import frozen
 from mecfs_bio.build_system.asset.base_asset import Asset
 from mecfs_bio.build_system.asset.file_asset import FileAsset
 from mecfs_bio.build_system.meta.asset_id import AssetId
+from mecfs_bio.build_system.meta.filtered_gwas_data_meta import FilteredGWASDataMeta
+from mecfs_bio.build_system.meta.gwas_summary_file_meta import GWASSummaryDataFileMeta
 from mecfs_bio.build_system.meta.meta import Meta
+from mecfs_bio.build_system.meta.read_spec.dataframe_read_spec import (
+    DataFrameParquetFormat,
+    DataFrameReadSpec,
+)
 from mecfs_bio.build_system.meta.read_spec.read_dataframe import scan_dataframe_asset
 from mecfs_bio.build_system.rebuilder.fetch.base_fetch import Fetch
 from mecfs_bio.build_system.task.base_task import Task
@@ -34,25 +40,25 @@ ChromPosStrategy = Literal["drop", "raise"]
 
 MismatchedAllelesStrategy = Literal["drop", "raise"]
 
-_IS_PALINDROMIC = "__is_palindromic__"
+IS_PALINDROMIC = "__is_palindromic__"
 
-_REFERENCE_EFFECT_ALLELE = "__reference_effect_allele__"
-_REFERENCE_NON_EFFECT_ALLELE = "__reference_non_effect_allele__"
-_REFERENCE_POS = "__reference_pos__"
-_REFERENCE_CHROM = "__reference_chrom__"
+REFERENCE_EFFECT_ALLELE = "__reference_effect_allele__"
+REFERENCE_NON_EFFECT_ALLELE = "__reference_non_effect_allele__"
+REFERENCE_POS = "__reference_pos__"
+REFERENCE_CHROM = "__reference_chrom__"
 
 _MISMATCH_POS_CHROM = "__MISMATCH_POS_CHROM__"
-_MATCH_REFERENCE = "__MATCH_REFERENCE__"
-_MATCH_REFERENCE_FLIPPED = "__MATCH_REFERENCE_FLIPPED__"
+MATCH_REFERENCE = "__MATCH_REFERENCE__"
+MATCH_REFERENCE_FLIPPED = "__MATCH_REFERENCE_FLIPPED__"
 
 
 _REF_COLS = [
-    _REFERENCE_EFFECT_ALLELE,
-    _REFERENCE_NON_EFFECT_ALLELE,
-    _REFERENCE_POS,
-    _REFERENCE_CHROM,
-    _MATCH_REFERENCE,
-    _MATCH_REFERENCE_FLIPPED,
+    REFERENCE_EFFECT_ALLELE,
+    REFERENCE_NON_EFFECT_ALLELE,
+    REFERENCE_POS,
+    REFERENCE_CHROM,
+    MATCH_REFERENCE,
+    MATCH_REFERENCE_FLIPPED,
     _MISMATCH_POS_CHROM,
 ]
 
@@ -65,7 +71,7 @@ class HarmonizeGWASWithReferenceViaRSIDTask(Task):
     In this context harmonization means:
     For non-palindromic variants:
        - keep only variants where:
-          -- effect and non-effect allele match the reference
+          -- effect and non-effect allele match the reference, or
           --effect allele matches reference non-effect allele, and vice versa.  In this case, flip beta and invert odds ratio
 
     For palindromic variants:
@@ -130,10 +136,10 @@ class HarmonizeGWASWithReferenceViaRSIDTask(Task):
         assert GWASLAB_RSID_COL in reference.columns
         reference = reference.rename(
             {
-                GWASLAB_EFFECT_ALLELE_COL: _REFERENCE_EFFECT_ALLELE,
-                GWASLAB_NON_EFFECT_ALLELE_COL: _REFERENCE_NON_EFFECT_ALLELE,
-                GWASLAB_POS_COL: _REFERENCE_POS,
-                GWASLAB_CHROM_COL: _REFERENCE_CHROM,
+                GWASLAB_EFFECT_ALLELE_COL: REFERENCE_EFFECT_ALLELE,
+                GWASLAB_NON_EFFECT_ALLELE_COL: REFERENCE_NON_EFFECT_ALLELE,
+                GWASLAB_POS_COL: REFERENCE_POS,
+                GWASLAB_CHROM_COL: REFERENCE_CHROM,
             }
         )
 
@@ -145,52 +151,96 @@ class HarmonizeGWASWithReferenceViaRSIDTask(Task):
         )
 
         gd = gwas_data.with_columns(
-            _is_palindromic_expr(
+            is_palindromic_expr(
                 GWASLAB_EFFECT_ALLELE_COL, nea_col=GWASLAB_NON_EFFECT_ALLELE_COL
-            ).alias(_IS_PALINDROMIC)
+            ).alias(IS_PALINDROMIC)
         )
 
+        gd = gd.join(reference, on=GWASLAB_RSID_COL)
         if self.palindrome_strategy == "drop":
-            gd = gd.filter(~pl.col(_IS_PALINDROMIC))
+            logger.debug(f"Dropping {gd[IS_PALINDROMIC].sum()} palindromic variants\n")
+            gd = gd.filter(~pl.col(IS_PALINDROMIC))
         else:
             raise NotImplementedError(
                 f"mode {self.palindrome_strategy} not implemented"
             )
-
-        gd = gd.join(reference, on=GWASLAB_RSID_COL)
         gd = gd.with_columns(
             _mismatch_pos_or_chrom_expr(
                 pos_col=GWASLAB_POS_COL,
                 chrom_col=GWASLAB_CHROM_COL,
-                ref_pos_col=_REFERENCE_POS,
-                ref_chrom_col=_REFERENCE_CHROM,
+                ref_pos_col=REFERENCE_POS,
+                ref_chrom_col=REFERENCE_CHROM,
             ).alias(_MISMATCH_POS_CHROM),
             _match_reference_expr(
                 ea_col=GWASLAB_EFFECT_ALLELE_COL,
                 nea_col=GWASLAB_NON_EFFECT_ALLELE_COL,
-                ref_ea_col=_REFERENCE_EFFECT_ALLELE,
-                ref_nea_col=_REFERENCE_NON_EFFECT_ALLELE,
-            ).alias(_MATCH_REFERENCE),
+                ref_ea_col=REFERENCE_EFFECT_ALLELE,
+                ref_nea_col=REFERENCE_NON_EFFECT_ALLELE,
+            ).alias(MATCH_REFERENCE),
             _match_flipped_reference_expr(
                 ea_col=GWASLAB_EFFECT_ALLELE_COL,
                 nea_col=GWASLAB_NON_EFFECT_ALLELE_COL,
-                ref_ea_col=_REFERENCE_EFFECT_ALLELE,
-                ref_nea_col=_REFERENCE_NON_EFFECT_ALLELE,
-            ).alias(_MATCH_REFERENCE_FLIPPED),
+                ref_ea_col=REFERENCE_EFFECT_ALLELE,
+                ref_nea_col=REFERENCE_NON_EFFECT_ALLELE,
+            ).alias(MATCH_REFERENCE_FLIPPED),
         )
 
         gd = _handle_chrom_pos(gd, self.chrom_pos_strategy)
-        gd = _handle_flipped(gd)
+        gd = handle_flipped(gd)
         _report_matches(gd)
-        gd = _handle_mismatched_alleles(gd, strategy=self.mismatched_alleles_strategy)
+        gd = handle_mismatched_alleles(gd, strategy=self.mismatched_alleles_strategy)
         gd = gd.drop(_REF_COLS)
+        logger.debug(f"harmonized gwas has length: {len(gd)}")
         out_path = scratch_dir / "matched_to_ref.parquet"
         gd.write_parquet(out_path)
 
         return FileAsset(out_path)
 
+    @classmethod
+    def create(
+        cls,
+        asset_id: str,
+        gwas_data_task: Task,
+        reference_task: Task,
+        palindrome_strategy: PalindromeStrategy,
+        chrom_pos_strategy: ChromPosStrategy,
+        mismatched_alleles_strategy: MismatchedAllelesStrategy,
+        gwas_pipe: DataProcessingPipe = IdentityPipe(),
+        ref_pipe: DataProcessingPipe = IdentityPipe(),
+    ):
+        source_meta = gwas_data_task.meta
+        read_spec = DataFrameReadSpec(DataFrameParquetFormat())
+        meta: Meta
+        if isinstance(source_meta, FilteredGWASDataMeta):
+            meta = FilteredGWASDataMeta(
+                short_id=AssetId(asset_id),
+                trait=source_meta.trait,
+                project=source_meta.project,
+                sub_dir=source_meta.sub_dir,
+                read_spec=read_spec,
+            )
+        elif isinstance(source_meta, GWASSummaryDataFileMeta):
+            meta = GWASSummaryDataFileMeta(
+                short_id=AssetId(asset_id),
+                trait=source_meta.trait,
+                project=source_meta.project,
+                sub_dir=source_meta.sub_dir,
+                project_path=None,
+                read_spec=read_spec,
+            )
+        return cls(
+            meta=meta,
+            gwas_data_task=gwas_data_task,
+            reference_task=reference_task,
+            palindrome_strategy=palindrome_strategy,
+            chrom_pos_strategy=chrom_pos_strategy,
+            mismatched_alleles_strategy=mismatched_alleles_strategy,
+            gwas_pipe=gwas_pipe,
+            ref_pipe=ref_pipe,
+        )
 
-def _complement_reverse_expr(col_name: str) -> pl.Expr:
+
+def complement_reverse_expr(col_name: str) -> pl.Expr:
     return (
         pl.col(col_name)
         .str.replace_all("A", "X")
@@ -203,11 +253,11 @@ def _complement_reverse_expr(col_name: str) -> pl.Expr:
     )
 
 
-def _is_palindromic_expr(ea_col: str, nea_col: str) -> pl.Expr:
+def is_palindromic_expr(ea_col: str, nea_col: str) -> pl.Expr:
     """
     Check if a variant is palindromic.
     """
-    return pl.col(ea_col) == _complement_reverse_expr(nea_col)
+    return pl.col(ea_col) == complement_reverse_expr(nea_col)
 
 
 def _mismatch_pos_or_chrom_expr(
@@ -254,26 +304,26 @@ def _handle_chrom_pos(df: pl.DataFrame, strategy: ChromPosStrategy) -> pl.DataFr
     return df
 
 
-def _handle_flipped(
+def handle_flipped(
     df: pl.DataFrame,
 ) -> pl.DataFrame:
     df = df.with_columns(
-        pl.when(_MATCH_REFERENCE_FLIPPED)
+        pl.when(MATCH_REFERENCE_FLIPPED)
         .then(pl.col(GWASLAB_NON_EFFECT_ALLELE_COL))
         .otherwise(pl.col(GWASLAB_EFFECT_ALLELE_COL))
         .alias(GWASLAB_EFFECT_ALLELE_COL),
-        pl.when(_MATCH_REFERENCE_FLIPPED)
+        pl.when(MATCH_REFERENCE_FLIPPED)
         .then(pl.col(GWASLAB_EFFECT_ALLELE_COL))
         .otherwise(pl.col(GWASLAB_NON_EFFECT_ALLELE_COL))
         .alias(GWASLAB_NON_EFFECT_ALLELE_COL),
-        pl.when(_MATCH_REFERENCE_FLIPPED)
+        pl.when(MATCH_REFERENCE_FLIPPED)
         .then(-pl.col(GWASLAB_BETA_COL))
         .otherwise(pl.col(GWASLAB_BETA_COL))
         .alias(GWASLAB_BETA_COL),
     )
     if GWASLAB_ODDS_RATIO_COL in df.columns:
         df = df.with_columns(
-            pl.when(_MATCH_REFERENCE_FLIPPED)
+            pl.when(MATCH_REFERENCE_FLIPPED)
             .then(
                 1 / pl.col(GWASLAB_ODDS_RATIO_COL),
             )
@@ -282,7 +332,7 @@ def _handle_flipped(
         )
     if GWASLAB_EFFECT_ALLELE_FREQ_COL in df.columns:
         df = df.with_columns(
-            pl.when(_MATCH_REFERENCE_FLIPPED)
+            pl.when(MATCH_REFERENCE_FLIPPED)
             .then(1 - pl.col(GWASLAB_EFFECT_ALLELE_FREQ_COL))
             .otherwise(pl.col(GWASLAB_EFFECT_ALLELE_FREQ_COL))
             .alias(GWASLAB_EFFECT_ALLELE_FREQ_COL),
@@ -290,10 +340,10 @@ def _handle_flipped(
     return df
 
 
-def _handle_mismatched_alleles(
+def handle_mismatched_alleles(
     df: pl.DataFrame, strategy: MismatchedAllelesStrategy
 ) -> pl.DataFrame:
-    mismatched = (~(df[_MATCH_REFERENCE])) & (~(df[_MATCH_REFERENCE_FLIPPED]))
+    mismatched = (~(df[MATCH_REFERENCE])) & (~(df[MATCH_REFERENCE_FLIPPED]))
     num_mismatch = (mismatched).sum()
     msg = f"Found {num_mismatch} variants with matching rsid but mismatched alleles"
     if num_mismatch > 0:
@@ -305,9 +355,9 @@ def _handle_mismatched_alleles(
 
 
 def _report_matches(df: pl.DataFrame) -> None:
-    num_matches = df[_MATCH_REFERENCE].sum()
-    flip_matches = df[_MATCH_REFERENCE_FLIPPED].sum()
+    num_matches = df[MATCH_REFERENCE].sum()
+    flip_matches = df[MATCH_REFERENCE_FLIPPED].sum()
     logger.debug(
-        f"Found {num_matches} variants matching the reference in their base orientation"
+        f"Found {num_matches} variants matching the reference in their base orientation\n\n"
         f"Found {flip_matches} variants matching the reference when effect and non-effect alleles are flipped"
     )
